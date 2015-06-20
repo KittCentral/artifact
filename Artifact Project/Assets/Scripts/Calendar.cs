82a2fb0ba9b751537aa77840853cbde6dec14d69
chva1582 <chva1582@colorado.edu>
2015-06-20 14:33:11 -0700

    calendar polish

--- Artifact Project/Assets/Scripts/Calendar.cs
+++ Artifact Project/Assets/Scripts/Calendar.cs
@@ -11,37 +11,57 @@ using System.Collections.Generic;
 public class Calendar : MonoBehaviour 
 {
 	//Initialize the day squares
-	public GameObject[] eventObject = new GameObject[42];
 	public GameObject[] cubicles = new GameObject[42];
 	private Text[] eventText = new Text[42];
 	private Text[] dateNum = new Text[42];
+	private Image[] cubBackground = new Image[42];
 
 	//Further Initialization
 	public GameObject MonthObj;
+	public GameObject Background;
+	private Image BGImage;
 	private Text monthText;
 	private DateTime displayedMonth;
 	string nowFilename;
 	DateTime firstDayOfMonth;
 	int numOfWeek;
+	int counter;
 	public int eventFontSize;
 
+	private Color[] colorList ={new Color(172f/255f,210f/255f,223f/255f),new Color(223f/255f,172f/255f,192f/255f),new Color(160f/255f,238f/255f,158f/255f),new Color(224f/255f,180f/255f,225f/255f),new Color(150f/255f,154f/255f,234f/255f),new Color(223f/255f,221f/255f,172f/255f),new Color(255f/255f,129f/255f,129f/255f),new Color(159f/255f,231f/255f,189f/255f),new Color(250f/255f,255f/255f,156f/255f),new Color(255f/255f,223f/255f,156f/255f),new Color(165f/255f,165f/255f,165f/255f),new Color(255f/255f,255f/255f,255f/255f)};
 	private DayOfWeek[] dayList = new DayOfWeek[7]{DayOfWeek.Monday,DayOfWeek.Tuesday,DayOfWeek.Wednesday,DayOfWeek.Thursday,DayOfWeek.Friday,DayOfWeek.Saturday,DayOfWeek.Sunday};
 	private string[] monthList = new  string[12]{"January","February","March","April","May","June","July","August","September","October","November","December"};
-	Dictionary<Int32,string> events;
+	string[] events;
 
 	void Start()
 	{
 		for(int i=0; i<42; i++)
 		{
-			dateNum[i] = cubicles[i].GetComponent<Text>();
+			for(int j=0; j<cubicles[i].transform.childCount;j++)
+			{
+				if(cubicles[i].transform.GetChild(j).transform.name=="Date")
+				{
+					dateNum[i] = cubicles[i].transform.GetChild(j).transform.GetComponent<Text>();
+				}
+				if(cubicles[i].transform.GetChild(j).transform.name=="Events")
+				{
+					eventText[i] = cubicles[i].transform.GetChild(j).transform.GetComponent<Text>();
+				}
+			}
+			cubBackground[i] = cubicles[i].GetComponent<Image>();
 		}
-		for(int i=0; i<42; i++)
-		{
-			eventText[i] = eventObject[i].GetComponent<Text>();
-		}
+		BGImage = Background.GetComponent<Image> ();
 		monthText = MonthObj.GetComponent<Text> ();
 		displayedMonth = DateTime.Now;
 		DisplayMonth ();
+	}
+
+	void Update()
+	{
+		if(counter != 0)
+		{
+			counter -= 1;
+		}
 	}
 
 	//This needs cleanup but its the place where the dictionary is initialized and the squares are filled with data
@@ -88,7 +108,7 @@ public class Calendar : MonoBehaviour
 		}
 		for(int i=1; i<=System.DateTime.DaysInMonth(displayedMonth.Year,displayedMonth.Month); i++)
 		{
-			string dailyEvents = events[i];
+			string dailyEvents = events[i-1];
 			string[] split = dailyEvents.Split(new char[]{'.'});
 			eventText[i-1+numOfWeek].text = " ";
 			for (int j=0; j<split.Length; j++)
@@ -98,53 +118,54 @@ public class Calendar : MonoBehaviour
 		}
 	}
 
-	public void Serialize(Dictionary<Int32,string> dictionary, Stream stream)
+	public void Serialize(string[] monthEvents, Stream stream)
 	{
 		BinaryWriter writer = new BinaryWriter (stream);
-
-		writer.Write (dictionary.Count);
-		foreach(var events in dictionary)
+		string output;
+		if(monthEvents.Length > 0)
 		{
-			writer.Write(events.Key);
-			writer.Write(events.Value);
+			output = monthEvents[0];
 		}
+		else
+		{
+			output = "";
+		}
+		for(int i=1;i<monthEvents.Length;i++)
+		{
+			output = output + "," + monthEvents[i];
+		}
+		writer.Write (output);
 		writer.Flush ();
 	}
 
-	public Dictionary<Int32,string> OpenMonth(DateTime dateTime)
+	public string[] OpenMonth(DateTime dateTime)
 	{
 		string filename = "Assets/Shortcuts/MonthData/" + monthList [dateTime.Month] + dateTime.Year + ".dat";
 		var file = File.Open (filename, FileMode.Open);
 		BinaryReader reader = new BinaryReader (file);
-		int count = reader.ReadInt32 ();
-		var dictionary = new Dictionary<Int32,string> (count);
-		for (int i = 0; i < count; i++)
-		{
-			var key = reader.ReadInt32();
-			var value = reader.ReadString();
-			dictionary.Add(key,value);
-		}
+		string input = reader.ReadString();
+		string[] monthEvents = input.Split(new char[]{','});
 		file.Close ();
-		return dictionary;
+		return monthEvents;
 	}
 
 	public void AddEvent(string date)
 	{
 		string[] split = date.Split(new char[]{'.'});
 		DateTime dateTime = new DateTime (Convert.ToInt32(split [0]), Convert.ToInt32(split [1]), 1);
-		Dictionary<Int32,string> dict = OpenMonth (dateTime);
-		string stuff = dict [Convert.ToInt32(split [2])];
+		string[] monthEvents = OpenMonth (dateTime);
+		string stuff = monthEvents [Convert.ToInt32(split [2])];
 		if (!Convert.ToBoolean(String.Compare(stuff,"")))
 		{
-			dict[Convert.ToInt32(split[2])] = split[3];
+			monthEvents[Convert.ToInt32(split[2])] = split[3];
 		}
 		else
 		{
-			dict[Convert.ToInt32(split[2])] = stuff + "." + split[3];
+			monthEvents[Convert.ToInt32(split[2])] = stuff + "." + split[3];
 		}
 		string filename = "Assets/Shortcuts/MonthData/" + monthList [Convert.ToInt32(split[1])] + split[0] + ".dat";
 		var file = File.Open (filename, FileMode.Open);
-		Serialize (dict, file);
+		Serialize (monthEvents, file);
 		file.Close ();
 	}
 
@@ -152,44 +173,32 @@ public class Calendar : MonoBehaviour
 	{
 		string[] split = date.Split(new char[]{'.'});
 		DateTime dateTime = new DateTime (Convert.ToInt32(split [0]), Convert.ToInt32(split [1]), 1);
-		Dictionary<Int32,string> dict = OpenMonth (dateTime);
-		dict[Convert.ToInt32(split[2])] = "";
+		string[] monthEvents = OpenMonth (dateTime);
+		monthEvents[Convert.ToInt32(split[2])] = "";
 		string filename = "Assets/Shortcuts/MonthData/" + monthList [Convert.ToInt32(split[1])] + split[0] + ".dat";
 		var file = File.Open (filename, FileMode.Open);
-		Serialize (dict, file);
+		Serialize (monthEvents, file);
 		file.Close ();
 	}
 
 	public void AddMonth(int change)
 	{
-		for(int i = 0; i < change; i++)
+		if (counter == 0)
 		{
-			if(displayedMonth.Month != 12)
-			{
-				displayedMonth = new DateTime(displayedMonth.Year, displayedMonth.Month + 1, 1);
-			}
-			else
-			{
-				displayedMonth = new DateTime(displayedMonth.Year + 1, 1, 1);
-			}
+			displayedMonth = displayedMonth.AddMonths (change);
+			DisplayMonth();
 		}
-		DisplayMonth();
+		counter = 5;
 	}
 
 	public void LoseMonth(int change)
 	{
-		for(int i = 0; i < change; i++)
+		if (counter == 0)
 		{
-			if(displayedMonth.Month != 1)
-			{
-				displayedMonth = new DateTime(displayedMonth.Year, displayedMonth.Month - 1, 1);
-			}
-			else
-			{
-				displayedMonth = new DateTime(displayedMonth.Year - 1, 12, 1);
-			}
+			displayedMonth = displayedMonth.AddMonths (-1*change);
+			DisplayMonth();
 		}
-		DisplayMonth();
+		counter = 5;
 	}
 
 	void DisplayMonth()
@@ -208,12 +217,12 @@ public class Calendar : MonoBehaviour
 		if(!File.Exists(nowFilename))
 		{
 			var file = File.Open (nowFilename, FileMode.CreateNew);
-			Dictionary<Int32,string> dict = new Dictionary<int, string>();
+			string[] monthEvents = new string[System.DateTime.DaysInMonth(displayedMonth.Year,displayedMonth.Month)];
 			for(int i=0; i<System.DateTime.DaysInMonth(displayedMonth.Year,displayedMonth.Month);i++)
 			{
-				dict.Add(i+1,"");
+				monthEvents[i] = "";
 			}
-			Serialize(dict,file);
+			Serialize(monthEvents,file);
 			file.Close();
 		}
 	}
@@ -227,11 +236,15 @@ public class Calendar : MonoBehaviour
 			{
 				int date = i-numOfWeek+1;
 				dateNum[i].text = date.ToString();
+				cubBackground[i].fillCenter = false;
 			}
 			else
 			{
 				dateNum[i].text = " ";
+				cubBackground[i].fillCenter = true;
 			}
 		}
+		print (displayedMonth.Month);
+		BGImage.color = colorList [displayedMonth.Month - 1];
 	}
 }
