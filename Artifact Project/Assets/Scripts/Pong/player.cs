f864582a7b7f4a672f4e4ef1162bdbdf7cb32afc
GlobalCorsair <chva1582@colorado.edu>
2015-09-30 23:42:55 -0600

    pong cleanup

---
+++ Artifact Project/Assets/Scripts/Pong/player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Pong
+{
+	public class player : MonoBehaviour 
+	{
+		public Rigidbody body;
+		public KeyCode up, down, left, right, spinR, spinL;
+		public int lowerBoundY, upperBoundY;
+		
+		void Start()
+		{
+			body = GetComponent<Rigidbody>();
+		}
+
+		void Update () 
+		{
+			if(transform.position.x >= lowerBoundY && transform.position.x <= upperBoundY)
+			{
+				if(Input.GetKey(left))
+					body.AddForce(-10,0,0,ForceMode.Acceleration);
+
+				if(Input.GetKey(right))
+					body.AddForce(10,0,0,ForceMode.Acceleration);
+			}
+
+			else if(transform.position.x < lowerBoundY)
+				body.AddForce(-5*(transform.position.x-lowerBoundY),0,0,ForceMode.Acceleration);
+
+			else
+				body.AddForce(-5*(transform.position.x-upperBoundY),0,0,ForceMode.Acceleration);
+
+			if(Input.GetKey(up))
+				body.AddForce(0,0,20,ForceMode.Acceleration);
+
+			if(Input.GetKey(down))
+				body.AddForce(0,0,-20,ForceMode.Acceleration);
+
+			if(Input.GetKey(spinL))
+				body.AddTorque(0,30,0);
+
+			if(Input.GetKey(spinR))
+				body.AddTorque(0,-30,0);
+
+		}
+	}
+}
---
+++ Artifact Project/Assets/Scripts/Pong/scoreKeeper.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Pong
+{
+	public class scoreKeeper : MonoBehaviour 
+	{
+		//variable assignment
+		#region 
+		public KeyCode reset;
+		public GameObject ball;
+		public Rigidbody ballBody;
+		public GameObject p1;
+		public Rigidbody p1Body;
+		public GameObject p2;
+		public Rigidbody p2Body;
+		public Text scoreboard;
+		public GameObject explosion;
+		public int direction;
+		int dir;
+		
+		Vector3 rot = new Vector3(0,0,0);
+		int score;
+		#endregion
+
+		void Start () 
+		{
+			ballBody = ball.GetComponent<Rigidbody>();
+			p1Body = p1.GetComponent<Rigidbody>();
+			p2Body = p2.GetComponent<Rigidbody>();
+			ballBody.AddForce(2.5f,0,0,ForceMode.VelocityChange);
+		}
+		
+		void OnTriggerEnter(Collider col)
+		{
+			if(col.gameObject.tag == "Ball")
+			{
+				Instantiate(explosion, ball.transform.position, Quaternion.Euler(ball.transform.eulerAngles.x + 90, ball.transform.eulerAngles.y, ball.transform.eulerAngles.z));
+				StartCoroutine(ResetBall());
+				ResetPlayers();
+
+				score += 10;
+				scoreboard.text = score.ToString();
+			}
+		}
+
+		void Update()
+		{
+			dir = direction/Mathf.Abs(direction);
+
+			if(Input.GetKeyDown(reset))
+			{
+				StartCoroutine(ResetBall());
+				ResetPlayers();
+			}
+		}
+
+		void ResetPlayers()
+		{
+			p1.transform.position = new Vector3(7,0,0);
+			p1.transform.eulerAngles = rot;
+			p1Body.velocity = Vector3.zero;
+			p1Body.angularVelocity = Vector3.zero;
+			
+			p2.transform.position = new Vector3(-7,0,0);
+			p2.transform.eulerAngles = rot;
+			p2Body.velocity = Vector3.zero;
+			p2Body.angularVelocity = Vector3.zero;
+		}
+
+		IEnumerator ResetBall()
+		{
+			ball.transform.position = Vector3.zero;
+			ballBody.velocity = new Vector3(0,0,0);
+			yield return new WaitForSeconds(1);
+			ballBody.velocity = new Vector3(5*dir,0,0);
+		}
+	}
+}
