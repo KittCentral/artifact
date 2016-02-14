f1aabecee28dea30f547108119ec72987e8c0545
Global Asus 1 <chva1582@colorado.edu>
2016-02-14 00:16:53 -0700

    Value Noise

---
+++ Artifact Project/Assets/Scripts/Tools/TextureCreator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class TextureCreator : MonoBehaviour
+{
+    [Range(2, 512)]
+    public int resolution = 256;
+    public float frequency = 1f;
+    [Range(1, 3)]
+    public int dimensions = 3;
+    public Procedural.NoiseMethodType type;
+    Texture2D texture;
+
+	void OnEnable()
+    {
+        if (texture == null)
+        {
+            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+            texture.name = "Procedural Texture";
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Trilinear;
+            texture.anisoLevel = 9;
+            GetComponent<MeshRenderer>().material.mainTexture = texture;
+        }
+        FillTexture();
+    }
+
+    public void FillTexture ()
+    {
+        if (texture.width != resolution)
+            texture.Resize(resolution, resolution);
+
+        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
+        Vector3 point01 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+        Vector3 point10 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+        Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+
+        //Debug.Log(Procedural.Noise.noiseMethods[0]);
+        Procedural.NoiseMethod method = Procedural.Noise.noiseMethods[(int)type][dimensions - 1];
+        for (int y = 0; y < resolution; y++)
+        {
+            Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) / resolution);
+            Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) / resolution);
+            for (int x = 0; x < resolution; x++)
+            {
+                Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) / resolution);
+                float sample = method(point, frequency);
+                if (type != Procedural.NoiseMethodType.Value)
+                    sample = sample * 0.5f + 0.5f;
+                texture.SetPixel(x, y, Color.white * sample);
+            }
+        }
+        texture.Apply();
+    }
+
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            transform.hasChanged = false;
+            FillTexture();
+        }
+    }
+}
