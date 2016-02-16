69a7286d73820d96884e4cd515f0c50f362514e1
chva1582 <chva1582@colorado.edu>
2016-02-15 18:19:09 -0700

    Revert "Main menu"

---
+++ Artifact Project/Assets/DecalCreator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecalCreator : MonoBehaviour
+{
+    public GameObject splatOriginal;
+    GameObject clone;
+
+    void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            clone = Instantiate(splatOriginal, new Vector3(0, 0.5f, -2), Quaternion.Euler(new Vector3(90, 0, 0))) as GameObject;
+        }
+	}
+}
