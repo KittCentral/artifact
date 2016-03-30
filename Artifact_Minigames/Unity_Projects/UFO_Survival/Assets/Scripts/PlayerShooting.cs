857309a832fd5ff1a6b2e4e0c13e8bf5f1c4ce9c
georgeallison <georgeallison8@gmail.com>
2016-03-29 18:09:48 -0600

    Added Artifact Minigames Folder

---
+++ Artifact_Minigames/Unity_Projects/UFO_Survival/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public float timeBetweenBullets = 0.5f;
+    public float range = 100f;
+
+    float timer;
+    LineRenderer gunLine;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+    Camera thisCamera;
+    Vector3 newPosition;
+
+
+    void Awake ()
+    {
+        thisCamera = Camera.main;
+        gunLine = GetComponent <LineRenderer> ();
+        gunLight = GetComponent<Light> ();
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shoot ();
+        }
+
+        if(timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects ();
+        }
+    }
+
+
+    public void DisableEffects ()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+
+    void Shoot ()
+    {
+        timer = 0f;
+        gunLight.enabled = true;
+        gunLine.enabled = true;
+        gunLine.SetPosition (0, transform.position);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = hit.point;
+                gunLine.SetPosition(1, newPosition);
+            }
+            
+        }
+    }
+}
---
+++ Artifact_Minigames/Unity_Projects/UFO_Survival/Assets/Scripts/closeGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class closeGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+}
