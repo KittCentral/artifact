ec801547099a68ec375a359ba530bbe11fd3f33b
Global Asus 1 <chva1582@colorado.edu>
2016-04-11 12:41:02 -0600

    platform jump prototype

---
+++ Artifact Project/Assets/PlatformControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace PlatformJump
+{
+    public class PlatformControl : MonoBehaviour
+    {
+        public GameObject platformPrefab;
+        public int platCount;
+        public float speed;
+
+        // Use this for initialization
+        void Start()
+        {
+            for (int i = 0; i < platCount; i++)
+            {
+                GameObject plat = Instantiate(platformPrefab, new Vector3(10 * i, -1), Quaternion.identity) as GameObject;
+                plat.transform.parent = transform;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            for (int i = 1; i < platCount + 1; i++)
+            {
+                Transform trans = GetComponentsInChildren<Transform>()[i].transform;
+                trans.Translate(-1 * speed * Time.deltaTime, 0, 0);
+                if (trans.position.x < -10)
+                    trans.position = new Vector3(10 * (platCount - 1), Random.Range(-2f, 5f));
+            }
+        }
+    }
+}
---
+++ Artifact Project/Assets/Scripts/Platform Jump/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace PlatformJump
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Player : MonoBehaviour
+    {
+        public enum JumpTypes { singleJump, doubleJump, flying };
+        public JumpTypes jumpType = JumpTypes.singleJump;
+
+        public float jumpForce;
+        Rigidbody body;
+
+        int jump = -1;
+
+        // Use this for initialization
+        void Awake()
+        {
+            body = GetComponent<Rigidbody>();
+            JumpReset();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && jump != 0)
+                Jump();
+            if (transform.position.y < -5)
+                Fall();
+        }
+
+        void OnCollisionEnter (Collision col)
+        {
+            if(col.gameObject.tag == "Field")
+                JumpReset();
+        }
+
+        void Jump ()
+        {
+            body.AddTorque(Vector3.forward * 50 * -1);
+            body.AddForce(Vector3.up * 100 * jumpForce);
+            jump--;
+        }
+
+        void JumpReset ()
+        {
+            if (jumpType == JumpTypes.singleJump)
+                jump = 1;
+            else if (jumpType == JumpTypes.doubleJump)
+                jump = 2;
+        }
+
+        void Fall ()
+        {
+            body.velocity = Vector3.zero;
+            transform.position = new Vector3(0, 12, 0);
+        }
+    }
+}
