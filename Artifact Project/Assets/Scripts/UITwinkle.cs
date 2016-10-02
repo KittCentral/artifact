d26e8fcd31b696ea762ac9b55513b9f3b94d4d64
Global Asus 1 <chva1582@colorado.edu>
2016-10-02 17:19:16 -0600

    NewUI

---
+++ Artifact Project/Assets/Scripts/UITwinkle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UITwinkle : MonoBehaviour
+{
+    public float frequency;
+    public float percentAmplitude;
+    public Color targetColor;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float colorFraction = percentAmplitude * Mathf.Sin(frequency * Time.time) + (1-percentAmplitude);
+        transform.GetComponent<Image>().color = targetColor * colorFraction;
+	}
+}
---
+++ Artifact Project/Assets/UIControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public void SceneAdd(int i)
+    {
+        SceneControl.OpenSceneAdditive(i);
+    }
+
+    public void SceneOpen(int i)
+    {
+        SceneControl.OpenScene(i);
+    }
+}
