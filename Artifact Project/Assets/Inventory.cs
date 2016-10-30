f32cff7e51742b812cdfae4ce72be74719c7c7cd
Global Asus 1 <chva1582@colorado.edu>
2016-10-30 16:48:04 -0600

    playability

---
+++ Artifact Project/Assets/Inventory.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Voxel
+{
+    public class Inventory : MonoBehaviour
+    {
+        public Sprite[] blockThumbs;
+        public InventoryItem[] items = new InventoryItem[10];
+        public Dictionary<int, int> IDs = new Dictionary<int, int>(); //position, ID
+
+        public void AddItemButton(int position, InventoryItem item)
+        {
+            items[position] = item;
+        }
+
+        public Block GetBlock(int ID)
+        {
+            int block = transform.GetChild(ID).gameObject.GetComponent<InventoryItem>().BlockID;
+            switch (block)
+            {
+                case 0:
+                    return new BlockGrass();
+                case 1:
+                    return new BlockStone();
+                case 2:
+                    return new BlockWood();
+                case 3:
+                    return new BlockLeaves();
+                default:
+                    return null;
+            }
+        }
+
+        public void AddItem(int ID)
+        {
+            int position = 0;
+            if (IDs.ContainsValue(ID))
+            {
+                position = IDs.FirstOrDefault(x => x.Value == ID).Key;
+                items[position].Quantity++;
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < items.GetLength(0); i++)
+                {
+                    if (items[i].BlockID == -1)
+                    {
+                        items[i].BlockID = ID;
+                        items[i].Quantity++;
+                        IDs.Add(IDs.Count,ID);
+                        return;
+                    }
+                }
+            }
+        }
+
+        public bool RemoveItem(int position, out int blockID)
+        {
+            blockID = -1;
+            for (int i = 0; i < items.GetLength(0); i++)
+            {
+                if (IDs.TryGetValue(position, out blockID))
+                {
+                    if (blockID != -1)
+                    {
+                        items[position].Quantity--;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Artifact Project/Assets/InventoryItem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Voxel
+{
+    public class InventoryItem : MonoBehaviour
+    {
+        public int position;
+        int blockID;
+        int quantity;
+        Inventory inventory;
+        public bool isEmpty;
+
+        void Start()
+        {
+            blockID = -1;
+            inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
+            inventory.AddItemButton(position, this);
+        }
+
+        public int BlockID
+        {
+            get
+            {
+                return blockID;
+            }
+
+            set
+            {
+                blockID = value;
+                if(value != -1)
+                    transform.GetChild(1).GetComponent<Image>().sprite = inventory.blockThumbs[blockID];
+                transform.GetChild(1).gameObject.SetActive(blockID != -1);
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set
+            {
+                quantity = value;
+                if (value == 0)
+                {
+                    transform.GetChild(0).gameObject.GetComponent<Text>().text = "";
+                    isEmpty = true;
+                    BlockID = -1;
+                    inventory.IDs.Remove(position);
+                }
+                else
+                {
+                    transform.GetChild(0).gameObject.GetComponent<Text>().text = quantity.ToString();
+                    isEmpty = false;
+                }
+            }
+        }
+    }
+}
